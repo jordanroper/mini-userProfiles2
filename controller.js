fc12d20752b24be3195c397bd71040bc95cb08de
Jordan Roper <jordanroper@gmail.com>
2016-02-16 19:36:56 -0700

    Finished the project

--- controller.js
+++ controller.js
@@ -2,11 +2,23 @@ var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
 
+$scope.currentPage = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	$scope.users = mainService.getUsers($scope.currentPage).then(function(response){
+      $scope.users = response.data.data;
+    });
   }
-
-
   $scope.getUsers();
+
+$scope.next = function() {
+  $scope.currentPage++;
+  $scope.getUsers();
+}
+
+$scope.previous = function() {
+  $scope.currentPage--;
+  $scope.getUsers();
+}
 
 });
